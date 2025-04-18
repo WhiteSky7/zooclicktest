7cd37f4786532db461b13603769b456bfab66d0e
WhiteSky7 <satalk77@gmail.com>
2025-04-18 04:23:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('api/equipment', [App\Http\Controllers\EquipmentController::class, 'getEquipments']);
+
